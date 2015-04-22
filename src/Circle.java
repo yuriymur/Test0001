0b91dffba89bf150d5a7d4ee289ad388aa3f23a0
student <goal67@yandex.ru>
2015-04-22 16:51:14 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,43 @@
+/**
+ * Created by student on 22.04.2015.
+ */
+    public class Circle {           // save as "Circle.java"
+    // private instance variable, not accessible from outside this class
+    private double radius;
+    private String color;
+
+    // 1st constructor, which sets both radius and color to default
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    // 2nd constructor with given radius, but color default
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+
+    // A public method for retrieving the radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // A public method for computing the area of circle
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double r) {
+        radius = r;
+
+    }
+
+
+}
+
+
